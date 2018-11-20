6d3a55ae1fb8bb3aade5b49767d04db1cbdf30ca
krzysztoffigiel <krzysztoffigiel@MacBook-Pro-Krzysztof-2.local>
2018-11-20 11:09:32 +0100

    angular project & tensorflow install

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(){}
+
+}
